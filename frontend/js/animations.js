928fea19d3783711275b9cc9704e132fd73a9d89
Florian DAVILA <florian.davila@etu.umontpellier.fr>
2020-11-28 17:26:24 +0100

    Ajout validation formulaire de connexion

---
+++ frontend/js/animations.js
@@ -0,0 +1,23 @@
+// Fonctions onmouseover/out pour "Aide"
+
+function onUnderlinedText(obj)
+{
+    obj.classList.add('OMOUnderline')
+}
+
+function outUnderlinedText(obj)
+{
+    obj.classList.remove('OMOUnderline')
+}
+
+// Fonctions onmouseover/out pour le footer
+
+function onColoredText(obj)
+{
+    obj.classList.add('OMOColor')
+}
+
+function outColoredText(obj)
+{
+    obj.classList.remove('OMOColor')
+}
